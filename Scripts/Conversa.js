01c8348454fbc60096d297fe80a2b9ac478d5c6f
JohnGF <joaot.tomas415@gmail.com>
2021-12-01 21:36:25 +0000

    merge

---
+++ Scripts/Conversa.js
@@ -0,0 +1,84 @@
+"use strict";
+
+
+// window.addEventListener("load", principal);
+
+// function principal(){
+    
+// }
+
+function criaConversa(grupo){
+    if (contactos.includes(grupo) ){
+        document.getElementById("nomeGrupo").innerHTML = 
+        '<img src="Resources/Avatars/'+grupo+'A.png" height="60"/> ' + grupo
+    } else{
+        document.getElementById("nomeGrupo").innerHTML = 
+        '<img src="Resources/Avatars/user.png" height="60"/> ' + grupo
+    }
+}
+
+
+function abreConversa(nome){
+    nome = nome + ""
+    const a=document.querySelector(".tdConversa");
+    var reuniao = 'onclick="abreChamada(`'+ nome + '`)"';
+    var detalhes = 'onclick="abreDetalhes(`'+ nome + '`)"';
+    var newnode=document.createElement("td");
+    newnode.className="barraGrupo"
+    newnode.id="replaced";
+    newnode.className="tdConversa"
+    newnode.innerHTML=
+    '<div class="descricaoGrupo">'+
+    '<p id="nomeGrupo"></p>' +
+    '<img '+ reuniao + 'id="videocall" src="Resources/videocall.png" height="60"/>  '+
+    '<img '+ detalhes + 'id="more" src="Resources/more.png" height="60"/>  '+
+    '</div><div id = "conversa">'+
+    '<div id = "caixaConversa" class="scroll"></div>'+
+    '<textarea placeholder="Escreva uma mensagem..." id="mensagemConversa" required></textarea>'+
+    '<button type="submit" class="enviaMensagem" onclick="imprimeMensagem(`mensagemConversa`, `caixaConversa`)" style="height: 50px; width: 50px;"><img class="enviar" src="Resources/enviar.png"></button>'+
+    '<button type="button" class="enviaFicheiros" onclick= "enviaFicheiro(`mensagemConversa`, `caixaConversa`,`anexo1.pdf`)" style="height: 50px; width: 50px;"><img class="ficheiros" src="Resources/anexo.png"></button>'+
+    '</div>';
+ 
+    a.replaceWith(newnode);
+    criaConversa(nome)
+    
+}
+
+
+function adicionaLista(nomeGrupo){ 
+    var grupo = document.createTextNode(nomeGrupo);
+    var div = document.getElementById('grupos');
+    var paragrafo = document.createElement("a");
+    paragrafo.setAttribute('href',"");
+    paragrafo.appendChild(grupo);
+    div.appendChild(paragrafo);
+}
+
+
+function imprimeMensagem(id1,id2){  
+    var mensagem=document.getElementById(id1).value;
+    var resposta = document.createTextNode(mensagem);
+    var div = document.getElementById(id2);
+    var paragrafo = document.createElement("p");
+    paragrafo.setAttribute("class", "paragrafo");
+    paragrafo.appendChild(resposta);
+    div.prepend(paragrafo);
+    document.getElementById(id1).value = "";   
+    
+}
+    
+
+function enviaFicheiro(id1,id2, nomeFicheiro){
+    var resposta = document.createTextNode(nomeFicheiro);
+    var div = document.getElementById(id2);
+    var paragrafo = document.createElement("p");
+    paragrafo.setAttribute("class", "paragrafo");
+    paragrafo.setAttribute("id", "pdf");
+    paragrafo.appendChild(resposta);
+    div.prepend(paragrafo);
+    document.getElementById(id1).value = "";
+    var pdf = document.getElementById("pdf");
+    pdf.innerHTML = '<img class="enviar" src="Resources/pdf.png"> ' + pdf.innerHTML;
+}
+    
+
---
+++ Scripts/Reuniao.js
@@ -0,0 +1,107 @@
+"use strict";
+
+function abreChamada(nome){
+    nome = nome + ""
+    var mudaCamera = 'onclick="mudarCam(`'+ nome + '`)"';
+    var mudaMic = 'onclick="mudarMic(`'+ nome + '`)"';
+    var mudaAudio = 'onclick="mudarAudio(`'+ nome + '`)"';
+    var detalhes = 'onclick="abreDetalhes(`'+ nome + '`)"';
+    const a=document.querySelector(".tdConversa");
+    var newnode=document.createElement("td");
+    newnode.className="chamada tdConversa"
+    newnode.id="replaced";
+    newnode.innerHTML='<div class="functions">'+
+    '<a '+ mudaMic + '><img class="function" src='+mic+'></a>'
+    +'<a '+ mudaAudio + '><img class="function" src='+audio+'></a>'
+    +'<a '+ mudaCamera + '><img id="cam" class="function" src='+cam+'></a>'+
+    '<a onclick="abreChat()"><img class="function" src="Resources/chat.png"></a>'+
+    '<a onclick="compartilhaTela()"><img class="function" src="Resources/sharescreen.png"></a>'+
+    '<a id="nomeChamada" '+ detalhes + '>'+nome+'</a>'+
+    '<a href="IniciarReuniao.html" id = "leaveButton"><img class="function" src="Resources/leave.png"></a>'+
+    '</div>';
+    
+    var div=document.createElement("div");
+    div.id = "perfis"
+    div.innerHTML = mostraAvatares(nome)
+    newnode.appendChild(div)
+    a.replaceWith(newnode);
+   
+}
+
+function abreDetalhes(nome){
+    var div=document.createElement("div");
+    newnode.className="chamada tdConversa"
+    newnode.id="replaced";
+}
+
+
+function mostraAvatares(nome){
+
+    var result = '';
+    if (contactos.includes(nome)){
+        result = '<img src="Resources/Avatars/'+nome+'Img.jpeg" height="400"/>'
+    } else{
+        let part = grupos[nome];
+        for(var cada in part){
+            result += '<img src="Resources/Avatars/'+part[cada]+'Img.jpeg" height="200"/>' 
+        }
+        result += '<br>'
+    }
+    result += '<br>'
+    result += '<div><img src="Resources/Avatars/'+ imagemEscolhida+'" height="150"/></div>'
+    return result;
+}
+
+
+function compartilhaTela() {
+    if (document.getElementsByTagName("BODY")[0].style.border == "thick solid red") {
+        document.getElementsByTagName("BODY")[0].style.border = "none"
+    } else {
+        document.getElementsByTagName("BODY")[0].style.border = "thick solid red"
+    }
+}
+
+
+function abreChat(){
+    var x = document.getElementById("chat");
+    if (document.getElementById("chat").hidden == true) {
+        document.getElementById("chat").hidden = false;
+    } else {
+        document.getElementById("chat").hidden = true;
+    }
+}
+
+
+function mudarCam(nome){    
+    if (imagemEscolhida == "EuMascA.png") {
+        imagemEscolhida = "EuMascImg.jpeg"
+        cam = "Resources/cam.png"
+        localStorage.setItem('imagem', imagemEscolhida);
+
+    } else {
+        imagemEscolhida = "EuMascA.png"
+        cam = "Resources/noCam.png"
+        localStorage.setItem('imagem', imagemEscolhida);
+    }
+    abreChamada(nome)
+}
+
+function mudarMic(nome){    
+    if (mic == "Resources/Mute.png") {
+        mic = "Resources/mic.png"
+
+    } else {
+        mic = "Resources/Mute.png"
+    }
+    abreChamada(nome)
+}
+
+function mudarAudio(nome){    
+    if (audio == "Resources/Audio_mute.png") {
+        audio = "Resources/audio.png"
+
+    } else {
+        audio = "Resources/Audio_mute.png"
+    }
+    abreChamada(nome)
+}
